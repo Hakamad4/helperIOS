d6449ea26f587a2953c6e335b9ccc6cf278ecd4d
Hakamad4 <erik.hakamada@hotmail.com>
2017-03-29 21:10:00 -0300

    Commit inicial

---
+++ Helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helper
+//
+//  Created by Edson Hakamada on 29/03/17.
+//  Copyright © 2017 Erik Hakamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
